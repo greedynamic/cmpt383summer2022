f92b90e5f6316adf1633e5bce7655c2e6fb0dcfe
Your Name <you@example.com>
2022-05-30 18:09:37 -0700

    refactored code into functions

---
+++ Go_Assignment/rSort.go
@@ -0,0 +1,46 @@
+package main
+
+// import (
+// 	"math"
+// )
+
+func numsSort(nums []int) {
+	for i := range nums {
+		if i > 0 {
+			for preI := i; preI > 0; preI-- {
+				if nums[preI] < nums[preI-1] {
+					// swap
+					// temp := int(nums[preI])
+					// nums[preI] = nums[preI-1]
+					// nums[preI-1] = temp
+					nums[preI], nums[preI-1] = nums[preI-1], nums[preI]
+				}
+			}
+		}
+	}
+}
+
+func strsSort(strs []string) {
+	for i := range strs {
+		if i > 0 {
+			for preI := i; preI > 0; preI-- {
+				if strs[preI] < strs[preI-1] {
+					strs[preI], strs[preI-1] = strs[preI-1], strs[preI]
+				}
+			}
+		}
+	}
+}
+
+func rationalSort(rs []Rationalizer) {
+	for i := range rs {
+		if i > 0 {
+			for preI := i; preI > 0; preI-- {
+				if rs[preI].LessThan(rs[preI-1]) {
+					// if rs[preI].toFloat64() < rs[preI-1].toFloat64() {
+					rs[preI], rs[preI-1] = rs[preI-1], rs[preI]
+				}
+			}
+		}
+	}
+}
