d602ff2dbff0ebac99a84bdf2dbcc53617f55474
Haochen Yang <yuchun.li4096@gmail.com>
2022-07-21 00:35:54 -0700

    finish haskell

---
+++ Haskell_Assignment/temp.hs
@@ -0,0 +1,51 @@
+
+numLst4 <- randomList 4000
+numLst5 <- randomList 5000
+numLst6 <- randomList 6000
+numLst7 <- randomList 7000
+numLst8 <- randomList 8000
+numLst9 <- randomList 9000
+numLst10 <- randomList 10000
+insertionSort numLst4
+
+sLst1 <- randomStringList 1000
+sLst2 <- randomStringList 2000
+sLst3 <- randomStringList 3000
+sLst4 <- randomStringList 4000
+sLst5 <- randomStringList 5000
+sLst6 <- randomStringList 6000
+sLst7 <- randomStringList 7000
+sLst8 <- randomStringList 8000
+sLst9 <- randomStringList 9000
+sLst10 <- randomStringList 10000
+insertionSort sLst1
+
+rPair1 <- randomListPairs 1000
+insertionSort (randomRationalList rPair1)
+
+rPair2 <- randomListPairs 2000
+insertionSort (randomRationalList rPair2)
+
+rPair3 <- randomListPairs 3000
+insertionSort (randomRationalList rPair3)
+
+rPair4 <- randomListPairs 4000
+insertionSort (randomRationalList rPair4)
+
+rPair5 <- randomListPairs 5000
+insertionSort (randomRationalList rPair5)
+
+rPair6 <- randomListPairs 6000
+insertionSort (randomRationalList rPair6)
+
+rPair7 <- randomListPairs 7000
+insertionSort (randomRationalList rPair7)
+
+rPair8 <- randomListPairs 8000
+insertionSort (randomRationalList rPair8)
+
+rPair9 <- randomListPairs 9000
+insertionSort (randomRationalList rPair9)
+
+rPair10 <- randomListPairs 10000
+insertionSort (randomRationalList rPair10)
