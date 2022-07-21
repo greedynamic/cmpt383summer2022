6702cd279e67c14f05d0d098ef47f846f2576e80
Haochen Yang <yuchun.li4096@gmail.com>
2022-07-21 00:46:25 -0700

    added benchmark code

--- Haskell_Assignment/rational.hs
+++ Haskell_Assignment/rational.hs
@@ -157,14 +157,90 @@ insert x (y:ys) | x <= y    = x:y:ys
                 | otherwise = y : (insert x ys)
 
 insertionSort :: Ord a => [a] -> [a]
-isort []     = []
-isort (x:xs) = insert x (isort xs)
+insertionSort []     = []
+insertionSort (x:xs) = insert x (insertionSort xs)
+
+-- randomList: Int -> IO([Int])
+-- randomList 0 = return []
+-- randomList n = do r  <- randomRIO (1,10000)
+--                   rs <- randomList (n-1)
+--                   return (r:rs) 
+
+-- randomListPairs :: Int -> IO([(Int,Int)])
+-- randomListPairs 0 = return []
+-- randomListPairs n = do a <- randomRIO (1,10000)
+--                       b <- randomRIO (1,10000)
+--                       ps <- randomListPairs (n-1)
+--                       return ((a,b):ps) 
+
+-- randomStr :: Int -> IO String
+-- randomStr size = replicateM size $ randomRIO ('a', 'z')
+
+-- randomStrList :: Int -> IO([String])
+-- randomStrList 0 = return []
+-- randomStrList n = do r <- randomStr 10
+--                         rs <- randomStrList (n-1)
+--                         return (r:rs)
+
+-- randomRationalList :: [(Int,Int)] -> [MyRational]
+-- randomRationalList []           = []
+-- randomRationalList ((n,d):rest) = ((makeRational (toInteger n) (toInteger d)):(randomRationalList rest))
 
 --
 -- When you're ready to test insertionSort, put a main function here that
 -- calls it. See helloWorld.hs in the same folder for an example of how to do
 -- this.
 --
--- main = do putStrLn "Haskell!"
---           putStrLn "Calling insertionSort ..."
---
+-- main = do 
+-- putStrLn "Haskell!"
+-- putStrLn "Calling insertionSort ..."
+-- numLst4 <- randomList 4000
+-- numLst5 <- randomList 5000
+-- numLst6 <- randomList 6000
+-- numLst7 <- randomList 7000
+-- numLst8 <- randomList 8000
+-- numLst9 <- randomList 9000
+-- numLst10 <- randomList 10000
+-- insertionSort numLst4
+
+-- sLst1 <- randomStrList 1000
+-- sLst2 <- randomStrList 2000
+-- sLst3 <- randomStrList 3000
+-- sLst4 <- randomStrList 4000
+-- sLst5 <- randomStrList 5000
+-- sLst6 <- randomStrList 6000
+-- sLst7 <- randomStrList 7000
+-- sLst8 <- randomStrList 8000
+-- sLst9 <- randomStrList 9000
+-- sLst10 <- randomStrList 10000
+-- insertionSort sLst1
+
+-- rPair1 <- randomListPairs 1000
+-- insertionSort (randomRationalList rPair1)
+
+-- rPair2 <- randomListPairs 2000
+-- insertionSort (randomRationalList rPair2)
+
+-- rPair3 <- randomListPairs 3000
+-- insertionSort (randomRationalList rPair3)
+
+-- rPair4 <- randomListPairs 4000
+-- insertionSort (randomRationalList rPair4)
+
+-- rPair5 <- randomListPairs 5000
+-- insertionSort (randomRationalList rPair5)
+
+-- rPair6 <- randomListPairs 6000
+-- insertionSort (randomRationalList rPair6)
+
+-- rPair7 <- randomListPairs 7000
+-- insertionSort (randomRationalList rPair7)
+
+-- rPair8 <- randomListPairs 8000
+-- insertionSort (randomRationalList rPair8)
+
+-- rPair9 <- randomListPairs 9000
+-- insertionSort (randomRationalList rPair9)
+
+-- rPair10 <- randomListPairs 10000
+-- insertionSort (randomRationalList rPair10)
